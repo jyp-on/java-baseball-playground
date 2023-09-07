5356fe20d6c67138a945fcd993eb325b7a1fd0e0
mythpoy <okmlnsunok@gmail.com>
2023-09-07 13:12:18 +0900

    test : set API 테스트 완료

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,46 @@
+package study;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    void contains() {
+        assertThat(numbers.contains(1)).isTrue();
+        assertThat(numbers.contains(2)).isTrue();
+        assertThat(numbers.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void contains_withDeleteDuplicate(int number) {
+        assertThat(numbers.contains(number)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource({ "1, true", "2, true", "3, true", "4, false", "5, false" })
+    void csv_source_test(int input, boolean expectedResult) {
+        boolean actualResult = numbers.contains(input);
+        assertThat(actualResult).isEqualTo(expectedResult);
+    }
+}
